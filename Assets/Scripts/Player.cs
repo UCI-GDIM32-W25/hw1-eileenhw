f8129d655564049db17809f002db1cd554ddd03e
eileenhw <hwangem@uci.edu>
2025-01-09 10:48:44 -0800

    Movement and UI changes
    -Implemented movement
    -Implemented UI
    -Started on plants seeds function

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,16 +13,44 @@ public class Player : MonoBehaviour
 
     private void Start ()
     {
-        
+        _numSeedsLeft = 5;
+        _numSeedsPlanted = 0;
     }
 
     private void Update()
     {
-        
+        if(Input.GetKey(KeyCode.W))
+        {
+            _playerTransform.Translate(Vector3.up*_speed*Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            _playerTransform.Translate(Vector3.left*_speed*Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            _playerTransform.Translate(Vector3.down*_speed*Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            _playerTransform.Translate(Vector3.right*_speed*Time.deltaTime);
+        }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            PlantSeed();
+        }
     }
 
     public void PlantSeed ()
     {
-        
+       if(_numSeedsLeft > 0)
+       {
+            //make prefab appear
+            _numSeedsLeft--;
+            _numSeedsPlanted++;
+            //updateUI 
+            _plantCountUI.UpdateSeeds(_numSeedsLeft,_numSeedsPlanted);
+       }
+
     }
 }
